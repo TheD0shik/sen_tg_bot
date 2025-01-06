056688889224b0fb6f2f3803b194bf1f36ab98ee
дошик <the.doshik.exe@gmail.com>
2025-01-06 22:43:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 
-clientt =  '74a212327a64233a5997b05795f5596a85543629'
+clientt =  ''
  
 
-dbnamec="senko_tg_bot",
-userc="postgres",
-passwordc="The_D0shik",
-hostc="localhost"
+dbnamec="",
+userc="",
+passwordc="",
+hostc=""
 
 
 
-senko='6779540387:AAFzdZDrLe3Rnr4HEnDwjuRRIM6q5Yj5nfE'
+senko=''
